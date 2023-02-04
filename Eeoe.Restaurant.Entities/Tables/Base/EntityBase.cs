b5f80ea1e2ae4fdb610dbada29ce11275da67c50
Ozgur Fidan <ozgrfdn@gmail.com>
2023-02-04 15:45:31 +0300

    Entities.Tables.Base Oluşturuldu

---
+++ Eeoe.Restaurant.Entities/Tables/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace Eeoe.Restaurant.Entities.Tables.Base
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public string Ekleyen { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public String Duzenleyen { get; set; }
+        public DateTime DuzenlenmeTarihi { get; set; }
+    }
+}
