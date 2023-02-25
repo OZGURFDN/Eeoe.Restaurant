9137c8ad1397d2924a086cb56dbf77d79688f7de
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-02-25 14:32:10 +0300

    RestaurantContext Sınıfı oluşturuldu.

---
+++ Eeoe.Restaurant.DataAccess/Contexts/Restaurant/RestaurantConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Eeoe.Restaurant.DataAccess.Contexts.Restaurant
+{
+    public class RestaurantConfiguration : DbMigrationsConfiguration<RestaurantContext>
+    {
+        public RestaurantConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed=true;
+        }
+    }
+}
---
+++ Eeoe.Restaurant.DataAccess/Contexts/Restaurant/RestaurantContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Eeoe.Restaurant.DataAccess.Contexts.Base;
+using Eeoe.Restaurant.Entities.Tables;
+
+namespace Eeoe.Restaurant.DataAccess.Contexts.Restaurant
+{
+    public class RestaurantContext : BaseContext<RestaurantContext>
+    {
+        public RestaurantContext()
+        {
+
+        }
+
+        public RestaurantContext(string connectionString) : base(connectionString)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<RestaurantContext,RestaurantConfiguration>());
+        }
+
+        public DbSet<Urun> Urunler { get; set; }
+        public DbSet<Tanim> Tanimlar { get; set; }
+        public DbSet<Porsiyon> Porsiyonlar { get; set; }
+        public DbSet<EkMalzeme> EkMalzemeler { get; set; }
+    }
+}
