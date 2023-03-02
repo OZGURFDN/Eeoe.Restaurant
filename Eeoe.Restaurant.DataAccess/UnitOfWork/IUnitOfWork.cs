43029c0e10ba1f99fe2444af62b87ab4c08ea7c4
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-03-02 20:32:38 +0300

    IUnitOfWork

---
+++ Eeoe.Restaurant.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eeoe.Restaurant.DataAccess.Interfaces.Base;
+using Eeoe.Restaurant.Entities.Interfaces;
+
+namespace Eeoe.Restaurant.DataAccess.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TEntity> Dal<TEntity>() where TEntity : class,IEntity,new();
+
+        bool HasChanges();
+        bool DetectChanges();
+        bool Commit();
+    }
+}
---
+++ Eeoe.Restaurant.DataAccess/UnitOfWork/RestaurantUnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using Eeoe.Restaurant.DataAccess.Interfaces.Base;
+
+namespace Eeoe.Restaurant.DataAccess.UnitOfWork
+{
+    public class RestaurantUnitOfWork : IUnitOfWork
+    {
+        private bool disposedValue;
+
+        public bool Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DetectChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IRepository<TEntity> IUnitOfWork.Dal<TEntity>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~RestaurantUnitOfWork()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            System.GC.SuppressFinalize(this);
+        }
+    }
+}
